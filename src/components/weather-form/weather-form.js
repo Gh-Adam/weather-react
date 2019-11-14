2c711198904508cf6d7f9aecfa116cba72d7220a
ghada <ghada.adam.2011@gmail.com>
2019-11-14 15:24:04 +0200

    Start Form Stylying

--- src/components/weather-form/weather-form.js
+++ src/components/weather-form/weather-form.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
+import styles from "../../style.module.css";
 const WeatherForm = props => {
   return (
-    <form onSubmit={props.searchWeather}>
+    <form onSubmit={props.searchWeather} className={styles.weatherForm}>
       <input type="text" name="city" placeholder="Enter your City" />
       <input type="text" name="country" placeholder="Enter your Country" />
-      <button type="submit">Search</button>
+      <button type="submit" className={styles.submitBtn}>
+        Search
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default WeatherForm
+export default WeatherForm;
